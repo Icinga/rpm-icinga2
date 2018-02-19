Icinga/rpm-icinga2

commit 900b7cf3d9bba8e8c0d3fb38405a14ffe392a919

Fix duplication in file lists

icinga2.spec
@@ -673,13 +673,12 @@ fi
 %attr(0750,%{icinga_user},%{icingacmd_group}) %dir %{_localstatedir}/log/%{name}/compat
 %attr(0750,%{icinga_user},%{icingacmd_group}) %dir %{_localstatedir}/log/%{name}/compat/archives
 %attr(0750,%{icinga_user},%{icinga_group}) %{_localstatedir}/lib/%{name}
-%attr(0750,%{icinga_user},%{icingacmd_group}) %ghost %{_rundir}/%{name}
+%attr(0750,%{icinga_user},%{icingacmd_group}) %ghost %dir %{_rundir}/%{name}
 %attr(2750,%{icinga_user},%{icingacmd_group}) %ghost %{_rundir}/%{name}/cmd
 %attr(0750,%{icinga_user},%{icinga_group}) %dir %{_localstatedir}/spool/%{name}
 %attr(0770,%{icinga_user},%{icinga_group}) %dir %{_localstatedir}/spool/%{name}/perfdata
 %attr(0750,%{icinga_user},%{icinga_group}) %dir %{_localstatedir}/spool/%{name}/tmp
-%attr(0750,%{icinga_user},%{icinga_group}) %dir %{_datadir}/%{name}/include
-%{_datadir}/%{name}/include
+%attr(0750,%{icinga_user},%{icinga_group}) %{_datadir}/%{name}/include
 
 %files doc
 %defattr(-,root,root,-)

commit d8c8777b243f69743abf6680474953b4ec639552

Fix regression in include file list
The issue was introduced by the commit 900b7cf3d9bba8e8c0d3fb38405a14ffe392a919

The preceding attr was marking not only the directory but also the files
as executable.

icinga2.spec
@@ -678,7 +678,8 @@ fi
 %attr(0750,%{icinga_user},%{icinga_group}) %dir %{_localstatedir}/spool/%{name}
 %attr(0770,%{icinga_user},%{icinga_group}) %dir %{_localstatedir}/spool/%{name}/perfdata
 %attr(0750,%{icinga_user},%{icinga_group}) %dir %{_localstatedir}/spool/%{name}/tmp
-%attr(0750,%{icinga_user},%{icinga_group}) %{_datadir}/%{name}/include
+%attr(0750,%{icinga_user},%{icinga_group}) %dir %{_datadir}/%{name}/include
+%{_datadir}/%{name}/include/*
 
 %files doc
 %defattr(-,root,root,-)